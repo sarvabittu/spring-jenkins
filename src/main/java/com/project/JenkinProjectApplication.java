b980faab8a201348d83d449313b3ac0de38f94b5
Akash Singsarva <sarvabittu@gmail.com>
2024-08-18 20:31:10 +0530

    first commit for Jenkins Project

---
+++ src/main/java/com/project/JenkinProjectApplication.java
@@ -0,0 +1,26 @@
+package com.project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinProjectApplication {
+
+	public static final Logger logger = LoggerFactory.getLogger(JenkinProjectApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Post Construct logging...!");
+	}
+	
+	
+	public static void main(String[] args) {
+		logger.info("inside main springboot method logging...!");
+		SpringApplication.run(JenkinProjectApplication.class, args);
+	}
+
+}
